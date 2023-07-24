f3537db727e920ebb3914d368229b1d948912134
ZezetuSibara <zezethu0730@gmail.com>
2023-07-24 23:21:57 +0000

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -4,11 +4,10 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * struct listint_s - A doubly linked list node
+ * @n: An actual integer stored in the node
+ * @prev: Points to the previous element of the list
+ * @next: Points to the next element of the list
  */
 typedef struct listint_s
 {
@@ -16,12 +15,23 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/** The prototypes used for Sort Algorithms */
+void heap_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void quick_sort_hoare(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
 
