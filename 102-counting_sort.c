ec57040c5c00e1efb97b0825f69c2a8b1169f9e6
ZezetuSibara <zezethu0730@gmail.com>
2023-07-25 23:46:30 +0000

    0x1B. C - Sorting algorithms & Big O

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,88 +1,86 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
- * arr_zero_init - use 0 to initialize the array
- * @array: the innitialized array
- * @size: the actual array size
+ * _bigest -  Give me the largest number in a array of integers
+ * @array: The Int array
+ * @size: Size of array
+ * Return: The largest number
  */
-void arr_zero_init(int *array, int size)
+int _bigest(int *array, size_t size)
 {
-	int i;
+	size_t i;
+	int k = 0;
 
 	for (i = 0; i < size; i++)
-		array[i] = 0;
-}
-
-/**
- * get_max - the maximum value in the array is attanied
- * @array: the array to attain the value from
- * @size: the actual array size
- * Return: the array maximum value
- */
-int get_max(int *array, size_t size)
-{
-	int max;
-	size_t i;
-
-	if (size < 2)
-		return (0);
-
-	max = array[0];	/* first index */
-
-	for (i = 1; i < size; i++)
 	{
-		if (max < array[i])
-			max = array[i];
+		if (k < array[i])
+			k = array[i];
 	}
 
-	return (max);
+	return (k);
 }
-
 /**
- * counting_sort - will be used to sort an array
- * @array: the actual array
- * @size: the array size
+ * _memset -  Create a integer array and set each space in 0
+ * @size: Size of array
+ * Return: The integer array
+ */
+int *_memset(int size)
+{
+	int *ptr = NULL;
+	int i;
+
+	ptr = malloc((size) * sizeof(int));
+
+	for (i = 0; i < size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
+/**
+ * counting_sort - sort an array with the counting sort algorithm
+ * @array: The Int array
+ * @size: Size of array
+ * Return: The sorted list
  */
 void counting_sort(int *array, size_t size)
 {
-	int k = get_max(array, size), j, *position, *sumPosition, *sorted;
-	size_t i;
+	size_t i, j;
+	int k = 0;
+	int *ptr = NULL, *sort_ar = NULL;
 
-	if (k == 0)
+	if (size < 2)
 		return;
-
-	position = malloc(sizeof(int) * (k + 1));
-	if (!position)
+	/*Know the largest number in the array*/
+	k = _bigest(array, size);
+	/*Make the help array*/
+	ptr = _memset(k + 1);
+	if (!ptr)
 		return;
-	arr_zero_init(position, k + 1);
+	/*Set the values for sorting*/
+	for (i = 0; i < size; i++)
+		for (j = 0; (int)j < k + 1; j++)
+			if ((int)j == array[i])
+				ptr[j] += 1;
+	/*Modificate the count in the array*/
+	for (i = 0; (int)i < k; i++)
+		ptr[i + 1] = ptr[i] + ptr[i + 1];
+	print_array(ptr, k + 1);
+	/*Create the sort array*/
+	sort_ar = malloc(size * sizeof(int));
+	if (!sort_ar)
+	{
+		free(ptr);
+		return;
+	}
 
 	for (i = 0; i < size; i++)
-		position[array[i]] += 1;    /* one is set to the positions */
-
-	sumPosition = malloc(sizeof(int) * (k + 1));
-	if (!sumPosition)
-		return;
-	arr_zero_init(sumPosition, k + 1);
-	sumPosition[0] = position[0];   /* first index equal */
-
-	for (j = 1; j < (k + 1); j++)
-		sumPosition[j] = position[j] + sumPosition[j - 1];
-
-	free(position), print_array(sumPosition, k + 1);
-
-	sorted = malloc(sizeof(int) * size);
-	if (!sorted)
-		return;
-	arr_zero_init(sorted, size);
-
-	for (i = 0; i < size; i++)  /* the array gets sorted */
 	{
-		sumPosition[array[i]] -= 1;
-		sorted[sumPosition[array[i]]] = array[i];
+		sort_ar[ptr[array[i]] - 1] = array[i];
+		ptr[array[i]] -= 1;
 	}
-	free(sumPosition);
+	for (j = 0; j < size; j++)
+		array[j] = sort_ar[j];
 
-	for (i = 0; i < size; i++)  /* the array gets updated */
-		array[i] = sorted[i];
-	free(sorted);
+	free(sort_ar);
+	free(ptr);
 }
